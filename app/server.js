01b1b64a970161db408c859b394f1ab14c311062
deepakkum21 <deepakanjani21@gmail.com>
2020-12-30 23:53:00 +0530

    created a working docker-compose

--- app/server.js
+++ app/server.js
@@ -1,36 +1,56 @@
 const express = require("express");
 const mongoose = require("mongoose");
-//const ShortUrl = require('./models/shortUrl')
+const ShortUrl = require("./models/shortUrl");
 const app = express();
 
-console.log("object1");
+console.log(
+  "object1",
+  process.env.MONGODB_USERNAME +
+    "  " +
+    process.env.MONGODB_PASSWORD +
+    "  " +
+    process.env.PORT
+);
 
-// mongoose.connect('mongodb://localhost/urlShortener', {
-//   useNewUrlParser: true, useUnifiedTopology: true
-// })
+mongoose.connect(
+  "mongodb://host.docker.internal:27017/urlShortener",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.error("FAILED TO CONNECT TO MONGODB");
+      console.error(err);
+    } else {
+      console.log("CONNECTED TO MONGODB!!!");
+      app.listen(80);
+    }
+  }
+);
 
-// app.set('view engine', 'ejs')
-// app.use(express.urlencoded({ extended: false }))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-// app.get('/', async (req, res) => {
-//   const shortUrls = await ShortUrl.find()
-//   res.render('index', { shortUrls: shortUrls })
-// })
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
 
-// app.post('/shortUrls', async (req, res) => {
-//   await ShortUrl.create({ full: req.body.fullUrl })
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
 
-//   res.redirect('/')
-// })
+  res.redirect("/");
+});
 
-// app.get('/:shortUrl', async (req, res) => {
-//   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-//   if (shortUrl == null) return res.sendStatus(404)
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
 
-//   shortUrl.clicks++
-//   shortUrl.save()
+  shortUrl.clicks++;
+  shortUrl.save();
 
-//   res.redirect(shortUrl.full)
-// })
+  res.redirect(shortUrl.full);
+});
 
 app.listen(process.env.PORT || 5000);
